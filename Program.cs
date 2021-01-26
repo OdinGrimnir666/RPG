8f8e5293ef08d3fccf2293bc7552db6185c14ee3
OdinGrimnir666 <78020455+OdinGrimnir666@users.noreply.github.com>
2021-01-26 13:41:27 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RPG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var men = new Man();
+            Console.WriteLine(men.rasa);
+
+
+        }
+    }
+}
