1e5be16fe51477c9753babee1a5176f7b9545d08
OdinGrimnir666 <78020455+OdinGrimnir666@users.noreply.github.com>
2021-01-26 14:11:23 +0300

    Add files via upload

---
+++ Men.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPG
+{
+    class Man : BassKlass
+    {
+      
+        
+        public Man()
+        {
+           rasa= "Men";
+        }
+        public Man(string name, int lvl, int power, int xp, int domag, int armour, int money, int speedAtak,) 
+            : base(name,lvl,power, xp, domag, armour,  money, speedAtak) { }
+
+
+
+
+    }
+}
